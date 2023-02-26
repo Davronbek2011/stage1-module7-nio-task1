4be18a73270e07d15904e8b518b9402b4d28cffd
Davronbek Sharipov <davronking001@gmail.com>
2023-02-26 14:30:10 +0500

    Completed module7-nio-task1

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,41 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Path path = Paths.get(file.getPath());
+        Profile profile = new Profile();
+
+        try(BufferedReader reader = Files.newBufferedReader(path)){
+            String currLine = null;
+            while ((currLine = reader.readLine()) != null){
+                String[] str = currLine.split(": ");
+                switch (str[0]) {
+                    case "Name":
+                        profile.setName(str[1]);
+                        break;
+                    case "Age":
+                        profile.setAge(Integer.parseInt(str[1]));
+                        break;
+                    case "Email":
+                        profile.setEmail(str[1]);
+                        break;
+                    case "Phone":
+                        profile.setPhone(Long.parseLong(str[1]));
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return profile;
+        }catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
